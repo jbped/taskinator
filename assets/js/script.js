409ae023c11a5d5bbc81f76407dfcef0fb98fc4f
jbped <pedigojacob@gmail.com>
2021-05-10 22:56:01 -0600

    added ability to create a new task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
+var buttonEl = document.querySelector("#save-task");
+var taskToDoEl = document.querySelector("#tasks-to-do");
 
+var createTaskHandler = function () {
+    // Create new list item
+    var listItemEl = document.createElement("li");
+    // Style List item with matching CSS class
+    listItemEl.className = "task-item";
+    // Add text content to the new li item <li>TEXT CONTENT</li>
+    listItemEl.textContent = "This is a new item.";
+    // Create a new child which is the above listItemEl in the ul associated with taskToDoEl
+    taskToDoEl.appendChild(listItemEl);
+};
+
+// On button click with save-task id
+buttonEl.addEventListener("click", createTaskHandler);
+    
